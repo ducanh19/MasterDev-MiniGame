7c1722fb66fd05c7980e3750ded2dccb4c597870
ducanh19 <73572181+ducanh19@users.noreply.github.com>
2021-06-14 09:52:28 +0700

    Change ranking table display

--- db/player.php
+++ db/player.php
@@ -2,7 +2,7 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . '/quiz/db/connectDB.php');
 
 function getList() {
-    $sql = "SELECT * FROM `player`";
+    $sql = "SELECT * FROM `player` ORDER BY `Score` DESC";
     $result = $GLOBALS['conn']->query($sql);
     return $result->fetch_all(MYSQLI_ASSOC);
 }
